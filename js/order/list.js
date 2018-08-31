866b579eaa673b174333050aba80440703d11687
nidagewcc <nidagewcc@163.com>
2018-08-31 19:18:19 +0800

    新增景点相关页面

--- js/order/list.js
+++ js/order/list.js
@@ -99,6 +99,21 @@ new Vue({
 			pageSize: 10,
 			pageSizes: [10, 20],
 			currentPage: 1
+		},
+		dialogTableVisible: false,
+		dialogFormVisible: false,
+		form: {
+			desc: ''
+		},
+		formLabelWidth: '120px',
+		orderInfo: {
+			orderNo: 'A20180828183655',
+			contactName: '二狗',
+			contactPhone: '12354557125',
+			tripTitle: '北戴河两日游',
+			enrollTime: '2018-08-29 17:18:21',
+			ticketInfo: '成人票：2张 / 儿童票：1张 / 学生票：1张 / 其他：',
+			orderSts: '待处理'
 		}
 	},
 	methods: {
